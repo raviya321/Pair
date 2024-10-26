82aa5cd6914e81de7c047ba554e2329190fc72f8
raviya321 <159415931+raviya321@users.noreply.github.com>
2024-10-26 14:04:42 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -53,7 +53,7 @@ router.get('/', async (req, res) => {
                 await delay(5000);
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
-               let b64data = "RCD-MD&" + Buffer.from(data).toString('base64').substring(0, 6) + id.substring(0, 2);
+               let b64data = "RCD-MD&" + Buffer.from(data).toString('base64').substring(0, 8) + id.substring(0, 8);
                let session = await Pair_Code_By_Black_Castro.sendMessage(Pair_Code_By_Black_Castro.user.id, { text: '' + b64data });
 
                     let GHOST_MD_TEXT = `
